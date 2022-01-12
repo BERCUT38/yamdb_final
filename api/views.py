00ea9439eaf63ae739a67a0844d649dfdd2feac0
BERCUT38 <bazzz@list.ru>
2022-01-12 15:40:28 +0300

    2

--- api/views.py
+++ api/views.py
@@ -13,6 +13,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import Category, Genre, Title, User, Review
 from .permissions import (IsAdminOrSuperUser, IsAdminOrReadOnly,
                           IsAdminOrModeratorOrAuthor,)
+                          
 from .pagination import TitlePagination
 from .serializers import (
     CategorySerializer, GenreSerializer, SignUpSerializer,
